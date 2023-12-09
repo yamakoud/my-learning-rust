bedeaf600a6d45c2b18c91ec8b011fbbacb97b3e
kousei yamashita <kyamad9@gmail.com>
2023-12-09 17:08:02 +0900

    implement rectangle

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,9 +1,11 @@
 mod mains {
     // pub mod v1;
-    pub mod v2;
+    // pub mod v2;
+    pub mod v3;
 }
 
 fn main() {
     // mains::v1::main();
-    mains::v2::main();
+    // mains::v2::main();
+    mains::v3::main();
 }
--- 1/src/mains/v3.rs
+++ 1/src/mains/v3.rs
@@ -1,6 +1,22 @@
+trait Area {
+    fn area(&self) -> u32;
+}
 
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Area for Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+pub fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("rect area: {}", rect.area());
+}
